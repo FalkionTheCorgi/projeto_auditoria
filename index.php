99d4fcbb4ac3308d1b895606807927afdc364271
falkionthecorgi <manollomartinez@gmail.com>
2020-12-05 16:11:34 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+       <!-- <div class="row row_title">
+            <div class="col-lg-12 title">
+                <h2>ALGUM TITULO PARA O NOSSO SISTEMA </h2>
+            </div>     
+        </div> -->
+        <div class="row row_form">
+            <div class="caixa_login">
+                <form action = "">
+                    <!--<div class="col-lg-12"><img src="img/icon.png" alt="" class="logo"></div>-->
+                    <h3>Titulo do Sistema </h3>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Usuário</label>
+                        <input type="text" class="form-control" id="user" placeholder="Usuário">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Senha</label>
+                        <input type="password" class="form-control" id="password" placeholder="Senha">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Entrar</button>
+                </form>
+            </div>
+        </div>
+    </div>    
+</body>
+</html>
---
+++ login_success.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+       <!-- <div class="row row_title">
+            <div class="col-lg-12 title">
+                <h2>ALGUM TITULO PARA O NOSSO SISTEMA </h2>
+            </div>     
+        </div> -->
+        <div class="row row_sucesso">
+            <div class="caixa_sucesso">
+                <h1>LOGIN EFETUADO COM SUCESSO</h1>
+                <h4 class = "text-center">Bem Vindo, </h4>
+                <div class="col-lg-12">
+                    <h2>Nome: </h2>
+                    <h2>dado2: </h2>
+                    <h2>dado3: </h2>
+                    <h2>dado4: </h2>
+                    <h2>dado5: </h2>
+                </div>
+            </div>
+        </div>
+    </div>    
+</body>
+</html>
